f755dd61fac5ec2337769fae22c1cef4b7fffd16
Megha-Bathla <bathlamegha6@gmail.com>
2024-04-24 23:53:31 +0530

    refactor code

--- script.js
+++ script.js
@@ -2,16 +2,30 @@ let currentPlayer = "X";
 let arr = Array(9).fill(null);
 console.log(arr);
 
-function checkWinner(){
-    if((arr[0]!= null && arr[0]== arr[1] && arr[1]==arr[2]) || (arr[3]!= null && arr[3]== arr[4] && arr[4]==arr[5]) || (arr[6]!= null && arr[6]== arr[7] && arr[7]==arr[8]) || (arr[0]!= null && arr[0]== arr[3] && arr[3]==arr[6]) || (arr[1]!= null && arr[1]== arr[4] && arr[4]==arr[7]) || (arr[2]!= null && arr[2]== arr[5] && arr[5]==arr[8]) || (arr[0]!= null && arr[0]== arr[4] && arr[4]==arr[8]) || (arr[2]!= null && arr[2]== arr[4] && arr[4]==arr[6]) )
+function checkWinner() {
+  if (
+    (arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) ||
+    (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) ||
+    (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
+    (arr[0] !== null && arr[0] == arr[3] && arr[3] == arr[6]) ||
+    (arr[1] !== null && arr[1] == arr[4] && arr[4] == arr[7]) ||
+    (arr[2] !== null && arr[2] == arr[5] && arr[5] == arr[8]) ||
+    (arr[0] !== null && arr[0] == arr[4] && arr[4] == arr[8]) ||
+    (arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6])
+  ) {
+    console.log(currentPlayer);
+    document.write(`Winner is ${currentPlayer}🥳 `);
+  }
+  if (!arr.some((el) => el === null)) {
+    document.write(`Draw!! 😝`);
+  }
 }
 
 function handleClick(element) {
   let id = Number(element.id);
-  
+  if (arr[id] !== null) return;
   arr[id] = currentPlayer;
-  if(arr[id] )
   element.innerText = currentPlayer;
+  checkWinner();
   currentPlayer = currentPlayer === "X" ? "O" : "X";
-  console.log(arr);
 }
